87bbf463879879d684ff96db3ac5877e5287fd17
Obarski Maciej (TVN Digital) <mobarski@pl.grupa.iti>
2017-05-19 09:09:58 +0200

    p7 MK2 MOD0 - multiple pump mode

--- parallel/p7.py
+++ parallel/p7.py
@@ -1,4 +1,12 @@
+## p7.py - parallel processing microframework
+## (c) 2017 by mobarski (at) gmail (dot) com
+## licence: MIT
+## version: MK2 MOD0
+
 from __future__ import print_function
+
+# TODO - fix python2 error
+# TODO - refactor all run functions
 
 ###
 
@@ -49,14 +57,15 @@ def raw_gen(f,partition,block_size):
 import shlex
 import subprocess
 import time
-def run(cmd,f,cnt,out_prefix,out_suffix='',block_size=4096):
-	"run CMD in parallel batch on CNT partitions of line oriented file F"
+def run_batch(cmd,f,cnt,out_prefix,out_suffix='',block_size=4096):
+	"run CMD in parallel batch on CNT partitions of line oriented file F using single data pump"
 	t0=time.time()
 	generators = []
 	processes = []
 	meta_by_pid = {}
 	
 	### INIT ###
+	print('[BEGIN]\tpartitions={0} block_size={1}'.format(cnt,block_size))
 	f = open(f,'rb') if isinstance(f,str) else f
 	parts = partitions(f,cnt)
 	for i in range(cnt):
@@ -80,7 +89,7 @@ def run(cmd,f,cnt,out_prefix,out_suffix='',block_size=4096):
 		meta_by_pid[pid]['start_time']=time.time()
 		
 	### MAIN LOOP ### 
-	while generators:
+	while processes:
 		done = set()
 		for gen,proc in zip(generators,processes):
 			try:
@@ -101,16 +110,17 @@ def run(cmd,f,cnt,out_prefix,out_suffix='',block_size=4096):
 			proc.wait()
 	
 	### END STATISITCS ###
-	print('[END]\ttime={0:.2f}s'.format(time.time()-t0))
+	print('[END]\ttime={0:.2f}s partitions={1} block_size={2}'.format(time.time()-t0,cnt,block_size))
 
 
-def run_str(cmd,f,cnt,out_prefix,out_suffix='',block_size=4096):
+def run_stream(cmd,f,cnt,out_prefix,out_suffix='',block_size=4096):
 	"run CMD in parallel streaming on CNT partitions of line oriented input F"
 	t0=time.time()
 	processes = []
 	meta_by_pid = {}
 	
 	### INIT ###
+	print('[BEGIN]\tpartitions={0} block_size={1}'.format(cnt,block_size))
 	f = open(f,'rb') if isinstance(f,str) else f
 	for i in range(cnt):
 		f_out = open('{0}.out.part{1}{2}'.format(out_prefix,i,out_suffix),'w') 
@@ -119,7 +129,7 @@ def run_str(cmd,f,cnt,out_prefix,out_suffix='',block_size=4096):
 		proc = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=f_out, stderr=f_log)
 		processes += [proc]
 		### DIAGNOSTICS ###
-		print("[START]\tpartition={0} pid={1} block_size={2}".format(i, proc.pid, block_size))
+		print("[START]\tpartition={0} pid={1}".format(i, proc.pid))
 		pid = proc.pid
 		meta_by_pid[pid] = {}
 		meta_by_pid[pid]['partition']=i
@@ -145,11 +155,66 @@ def run_str(cmd,f,cnt,out_prefix,out_suffix='',block_size=4096):
 			proc.wait()
 	
 	### END STATISITCS ###
-	print('[END]\ttime={0:.2f}s'.format(time.time()-t0))
+	print('[END]\ttime={0:.2f}s partitions={1} block_size={2}'.format(time.time()-t0,cnt,block_size))
+
+import os
+def run_part(cmd,f,part_num,part_start,part_stop,out_prefix,out_suffix='',block_size=4096):
+	i = part_num
+	f = open(f,'rb') if isinstance(f,str) else f
+	f_out = open('{0}.out.part{1}{2}'.format(out_prefix,i,out_suffix),'w') 
+	f_log = open('{0}.log.part{1}{2}'.format(out_prefix,i,out_suffix),'w')
+	p = (part_start,part_stop)
+	gen = raw_gen(f,p,block_size)
+	args = shlex.split(cmd)
+	proc = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=f_out, stderr=f_log)
+	pid = proc.pid
+	print("[START]\tpartition={0} pid={2} pid_pump={1} todo={5} start={3} stop={4}".format(i,os.getpid(),pid,part_start,part_stop,part_stop-part_start))
+	sys.stdout.flush()
+	start_time=time.time()
+	done_bytes=0
+	
+	### MAIN LOOP ###
+	while True:
+		try:
+			block = next(gen)
+		except StopIteration:
+			proc.stdin.close()
+			break
+		proc.stdin.write(block)
+		done_bytes += len(block)
+	print("[DONE]\tpartition={0} pid={2} pid_pump={1} done={3} time={4:.2f}s".format(i,os.getpid(),pid,done_bytes,time.time()-start_time))
+
+import sys
+def run_batch2(cmd,f,cnt,out_prefix,out_suffix='',block_size=4096):
+	"run CMD in parallel batch on CNT partitions of line oriented file F using multiple data pumps"
+	t0=time.time()
+	print('[BEGIN]\tpartitions={0} block_size={1}'.format(cnt,block_size))
+	processes = []
+	f_name = f if isinstance(f,str) else f.name
+	f = open(f,'rb') if isinstance(f,str) else f
+	parts = partitions(f,cnt)
+	for i in range(cnt):
+		p = parts[i]
+		args = shlex.split('python p7.py')
+		proc = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=sys.stdout)
+		args_str = str([cmd,f_name,i,p[0],p[1],out_prefix,out_suffix,block_size])
+		proc.stdin.write(args_str.encode())
+		proc.stdin.close()
+		processes += [proc]	
+	for p in processes:
+		p.wait()
+
+	### END STATISITCS ###
+	print('[END]\ttime={0:.2f}s partitions={1} block_size={2}'.format(time.time()-t0,cnt,block_size))
+
+def run_part_main():
+	args_str = sys.stdin.read()
+	args = eval(args_str)
+	run_part(*args)
 
 ######################################################################################
 
-if __name__=="__main__":
-	run_str('python -c "import sys; print(sys.stdin.read())" ', 'test.txt', 4, 'test/test', '.txt', 5)
-	
+run = run_stream # default run function
 
+if __name__=="__main__":
+	run_part_main()
---
+++ parallel/p7_test_parts.py
@@ -0,0 +1,2 @@
+import p7
+p7.run_parts('python -c "import sys; print(sys.stdin.read())" ', 'test.txt', 4, 'test/test', '.txt', 5)
