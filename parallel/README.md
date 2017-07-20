edb38207586139bede27529e1fa6caf67e9e8a4b
Obarski Maciej (TVN Digital) <mobarski@pl.grupa.iti>
2017-07-20 13:45:01 +0200

    p7 --help

--- parallel/README.md
+++ parallel/README.md
@@ -11,6 +11,62 @@ The name (P7) is a numeronym (abbreviation) for the word **parallel**.
 ### P7 components
 
 **p7map** - mapper/combiner job executor
+
 **p7red** - reducer/combiner job executor
+
 **p7res** - resource manager
+
 **p7cat** - parallel concatenation of files
+
+
+### p7map
+
+```
+usage: p7x4m5.py [-h] [-i I] [-b B] [-o O] [-e E] [-p P] [-n N] mapper
+
+P7 streaming utility - run command in parallel
+
+positional arguments:
+  mapper      mapper command
+
+optional arguments:
+  -h, --help  show this help message and exit
+  -i I        head buffer size for input data pump (1024)
+  -b B        buffer size for subprocess (4096)
+  -o O        path template for stdout, must contain {0} which will be
+              replaced by partition id (mapper{0}.out)
+  -e E        path template for stderr (logs), must contain {} which will be
+              replaced by partition id (mapper{}.log)
+  -p P        ouput pipe command (TODO)
+  -n N        number of mapper jobs (4)
+```
+
+### p7red
+
+```
+usage: p7red.py [-h] [-i I] [-o O] [-e E] [-c C] reducer
+
+p7red - P7 reducer job executor
+
+positional arguments:
+  reducer     reducer command
+
+optional arguments:
+  -h, --help  show this help message and exit
+  -i I        paths for stdin
+  -o O        path for stdout
+  -e E        path for stderr (logs)
+  -c C        combiner command
+```
+
+### p7cat
+
+```
+TODO
+```
+
+### p7res
+
+```
+TODO
+```
--- parallel/p7red.py
+++ parallel/p7red.py
@@ -1,21 +1,43 @@
-# cli - p7red reducer output inputs...
+# cli - p7red reducer output inpaths...
 
 import io
 import sys
 from tempfile import TemporaryFile
 import subprocess
 
-cmd = 'python p7red.test.py' # sys.argv[0]
-outpath = 'p7red.out.txt' # sys.argv[1]
-inputs = ['p7red.test0.txt','p7red.test1.txt'] #sys.argv[2:]
-combiner = 'sort | sort'
+if 0:
+	cmd = 'python p7red.test.py'
+	outpath = 'p7red.out.txt'
+	inpaths = ['p7red.test0.txt','p7red.test1.txt']
+	logpath = ''
+	combiner = 'sort | sort'
+else:
+	if sys.version < '2.7':
+		import optparse
+		# TODO
+	else:
+		import argparse
+		parser = argparse.ArgumentParser(description="p7red - P7 reducer job executor")
+		parser.add_argument('reducer',type=str,help='reducer command')
+		parser.add_argument('-i',type=str,default='',help='paths for stdin')
+		parser.add_argument('-o',type=str,default='',help='path for stdout')
+		parser.add_argument('-e',type=str,default='',help='path for stderr (logs)')
+		parser.add_argument('-c',type=str,default='',help='combiner command')
+		# TODO: merge / sort controls (current=sort)
+		cmd_line_args = parser.parse_args()
+	cmd = cmd_line_args.reducer
+	outpath = cmd_line_args.o
+	inpaths = cmd_line_args.i
+	logpath = cmd_line_args.e
+	combiner = cmd_line_args.c
 
 
+fe = open(logpath,'wb') if logpath else sys.stderr
 all_data = ''
-for path in inputs:
+for path in inpaths:
 	fi = open(path,'rb')
 	if combiner:
-		data = subprocess.check_output(combiner, stdin=fi, shell=True)
+		data = subprocess.check_output(combiner, stdin=fi, stderr=fe, shell=True)
 	else:
 		data = fi.read()
 	fi.close()
@@ -28,7 +50,7 @@ lines.sort()
 fi = TemporaryFile()
 fi.write('\n'.join(lines).encode())
 fi.seek(0)
-fo = open(outpath,'wb')
-p = subprocess.Popen(cmd, stdin=fi, stdout=fo, shell=True)
+fo = open(outpath,'wb') if outpath else sys.stdout
+p = subprocess.Popen(cmd, stdin=fi, stdout=fo, stderr=fe, shell=True)
 #
 p.wait()
