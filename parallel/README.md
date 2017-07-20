7015f8d2e5a6bf7efc72da0b5814e6769719ed31
Obarski Maciej (TVN Digital) <mobarski@pl.grupa.iti>
2017-07-20 08:00:24 +0200

    first readme

---
+++ parallel/README.md
@@ -0,0 +1,17 @@
+# P7
+
+
+### P7 overview
+
+P7 is a parallelization microframework / set of utilities similar to Hadoop Streaming but it is limited to single machine only. 
+
+The name (P7) is a numeronym (abbreviation) for the word **parallel**.
+
+
+### P7 components
+
+**p7map** - mapper/combiner job executor
+**p7red** - reducer job executor
+**p7res** - resource manager
+**p7cat** - parallel concatenation of files
+
--- parallel/p7res.py
+++ parallel/p7res.py
@@ -24,12 +24,40 @@ import pickle
 LOCK_FILE_NAME = 'p7res.lock'
 FLAGS = os.O_CREAT | os.O_EXCL
 
-try:
-	fd = os.open(LOCK_FILE_NAME,FLAGS)
-	os.close(fd)
-except: # OSError / FileExistsError
-	print('FILE EXISTS')
+def lock_or_die_on_timeout():
+	# TODO
+	try:
+		fd = os.open(LOCK_FILE_NAME,FLAGS)
+		os.close(fd)
+	except: # OSError / FileExistsError
+		print('FILE EXISTS')
 
+def read_config(): pass
 
+def check_reservations(): pass
 
-os.remove(LOCK_FILE_NAME)
+def kill_old_reservations(): pass
+
+def check_resources(): pass
+
+def create_reservation(): pass
+
+def ouput_reservation(): pass
+
+def unlock():
+	os.remove(LOCK_FILE_NAME)
+
+#-------------------------------------------------------------------
+
+def main():
+	lock_or_die_on_timeout()
+	read_config()
+	check_reservations()
+	kill_old_reservations()
+	check_resources()
+	create_reservation()
+	ouput_reservation()
+	unlock()
+
+if __name__=="__main__":
+	main()
