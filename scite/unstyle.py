a796f20c8b54656a0aedbff167aaf896c5b21be9
Obarski Maciej (TVN Digital) <mobarski@pl.grupa.iti>
2017-10-07 23:30:01 +0200

    scite

---
+++ scite/unstyle.py
@@ -0,0 +1,10 @@
+import os
+import re
+
+for fn in os.listdir():
+	if not fn.endswith('.properties'): continue
+	raw = open(fn,'r').read()
+	n = len(re.findall('(?m)^style[.]',raw))
+	print(fn,n)
+	clean = re.sub('(?m)^style[.]','#~unstyle~#style.',raw)
+	open(fn,'w').write(clean)
