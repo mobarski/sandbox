9b48a6fecff724b411adaa4b1514a6411008bf8f
Maciej Obarski <mobarski@gmail.com>
2017-10-13 00:24:49 +0200

    kv x1m3 - key not serialized, glob selection

--- xhub/kv_x1m3.py
+++ xhub/kv_x1m3.py
@@ -1,21 +1,22 @@
 # encoding: UTF-8
 
-## KV databases family built on top of SQLite
-## (c) 2017 by mobarski (at) gmail (dot) com
-## licence: MIT
-## version: x1m2 (x-experimental, p-preview, r-release, m-modification)
+# KV database built on top of SQLite
+# (c) 2017 by mobarski (at) gmail (dot) com
+# licence: MIT
+# version: x1m3 (x-experimental, p-preview, r-release, m-modification)
+
+# CHANGES:
+# x1m3 - key is not serialized, glob key selection for iterators, class renamed
 
 from __future__ import print_function
 import sqlite3
 from zlib import compress,decompress
-#from pickle import loads,dumps
-#from cPickle import loads,dumps
+##from pickle import loads,dumps
+##from cPickle import loads,dumps
 from marshal import loads,dumps
 
-# TODO:
-# [ ] separate ser+de for key, benchmark
 
-class blob_kv:
+class KV:
 	"Key-Value database built on top of SQLite"
 	def __init__(self,path=':memory:',zlevel=0,protocol=2):
 		self.path = path
@@ -35,31 +36,45 @@ class blob_kv:
 		else:
 			return loads(str(x))
 	### CORE ###
-	def get(self,k,default=None):
-		key=self.ser(k)
+	def get(self,key,default=None):
 		results = self.conn.execute('select v from kv where k=?',(key,))
 		x = results.fetchone()
 		return self.de(x[0]) if x else default
-	def set(self,k,v):
-		key=self.ser(k)
+	def set(self,key,v):
 		val = self.ser(v)
 		self.conn.execute('insert or replace into kv values (?,?)',(key,val))
 	### ITER ###
-	def keys(self):
-		for k in self.conn.execute('select k from kv'):
-			yield self.de(k[0])
-	def values(self):
-		for v in self.conn.execute('select v from kv'):
-			yield self.de(v[0])
-	def items(self):
-		for k,v in self.conn.execute('select k,v from kv'):
-			yield self.de(k),self.de(v)
-	def count(self):
-		for x in self.conn.execute('select count(1) from kv'):
-			return x[0]
+	def keys(self,like=None):
+		if like:
+			for k in self.conn.execute('select k from kv where k glob ?',(like,)):
+				yield k[0]
+		else:
+			for k in self.conn.execute('select k from kv'):
+				yield k[0]
+	def values(self,like=None):
+		if like:
+			for v in self.conn.execute('select v from kv where k glob ?',(like,)):
+				yield self.de(v[0])
+		else:
+			for v in self.conn.execute('select v from kv'):
+				yield self.de(v[0])
+	def items(self,like=None):
+		if like:
+			for k,v in self.conn.execute('select k,v from kv where k glob ?',(like,)):
+				yield k,self.de(v)			
+		else:
+			for k,v in self.conn.execute('select k,v from kv'):
+				yield k,self.de(v)
+	def count(self,like=None):
+		if like:
+			for x in self.conn.execute('select count(1) from kv where k glob ?',(like,)):
+				return x[0]
+		else:
+			for x in self.conn.execute('select count(1) from kv'):
+				return x[0]
 	def update(self,items):
 		ser = self.ser
-		self.conn.executemany('insert or replace into kv values (?,?)',[(ser(k),ser(v)) for k,v in items]) # best-of-5:205k/s
+		self.conn.executemany('insert or replace into kv values (?,?)',[(k,ser(v)) for k,v in items]) # best-of-5:205k/s
 		##from itertools import starmap
 		##ser2 = lambda x:(ser(x[0]),ser(x[1]))
 		##ser3 = lambda k,v:(ser(k),ser(v))
@@ -68,26 +83,30 @@ class blob_kv:
 		##self.conn.executemany('insert or replace into kv values (?,?)',map(ser2,items)) # best-of-5:188k/s
 		##self.conn.executemany('insert or replace into kv values (?,?)',[map(ser,kv) for kv in items]) # best-of-5:168k/s
 		##[self.set(k,v) for k,v in items] # best-of-5:144k/s
-	def size_iter(self,limit=-1): # TODO rename?
-		for k,vs in self.conn.execute('select k,length(v) from kv order by length(v) desc limit ?',(limit,)):
-			yield self.de(k),vs
+	def remove(self,keys):
+		[self.delete(k) for k in keys]
+	def size_iter(self,like=None,limit=-1): # TODO rename?
+		if like:
+			for k,vs in self.conn.execute('select k,length(v) from kv where k glob ? order by length(v) desc limit ?',(like,limit)):
+				yield k,vs
+		else:
+			for k,vs in self.conn.execute('select k,length(v) from kv order by length(v) desc limit ?',(limit,)):
+				yield k,vs
 	### OTHER ###
 	def commit(self):
 		self.conn.commit()
 	def create(self):
 		self.conn.execute('create table if not exists kv (k blob,v blob)')
 		self.conn.execute('create unique index if not exists i_kv on kv (k)')		
-	def delete(self,k):
-		key=self.ser(k)
+	def delete(self,key):
 		self.conn.execute('delete from kv where k=?',(key,))
-	def truncate(self):
+	def clear(self):
 		self.conn.execute('drop table kv')
 		self.create()
 		self.vacuum()
 	def vacuum(self):
 		self.conn.execute('vacuum')
-	def size(self,k):
-		key=self.ser(k)
+	def size(self,key):
 		results = self.conn.execute('select length(v) from kv where k=?',(key,))
 		x = results.fetchone()
 		return x[0] if x else None
@@ -99,10 +118,10 @@ class blob_kv:
 	### ### ###
 
 def test_kv():
-	db = blob_kv('usunmnie.db')
-	#db.set('a','to jest test żółć')
+	db = KV('usunmnie.db')
+	##db.set('a','to jest test żółć')
 	db.set('a','to jest test')
-	#db.set('b',['to','jest','test','żółć'])
+	##db.set('b',['to','jest','test','żółć'])
 	db.set('b',['to','jest','test'])
 	db.set('c',{'to':1,'jest':2,'test':3})
 	db.commit()
@@ -116,7 +135,7 @@ def test_kv():
 	print(db.count())
 	db.delete('a')
 	print(db.count())
-	db.truncate()
+	db.clear()
 	print(db.count())
 	db.commit()
 	db['x'] = 42
@@ -126,11 +145,12 @@ def test_kv():
 
 def bench_kv(N,mode='write'):
 	from time import time
-	#db = blob_kv('bench.db',0)
-	db = blob_kv()
+	db = KV('bench.db',0)
+	#db = KV()
 	data = {'k'+str(i):'v'+str(i) for i in range(N)}
+	#data = {i:'v'+str(i) for i in range(N)}
 	if mode=='write':
-		db.truncate()
+		db.clear()
 		db.commit()
 		t0=time()
 		db.update(data.items())
@@ -138,14 +158,18 @@ def bench_kv(N,mode='write'):
 		db.commit()
 		t2=time()
 		print('WRITE rows/s',int(N/(t2-t0)), "total {0:.2f}s".format(t2-t0), "update {0:.2f}s".format(t1-t0), "commit {0:.2f}s".format(t2-t1))
+		db.remove(db.keys())
 	if mode=='read':
 		t0=time()
 		[db.get(k) for k in data]
 		t1=time()
 		print('READ rows/s',int(N/(t1-t0)), "total {0:.2f}s".format(t1-t0))
-	
+	if mode=='keys':
+		t0=time()
+		[k for k in db.keys()][:10]
+		t1=time()
+		print('KEYS rows/s',int(N/(t1-t0)), "total {0:.2f}s".format(t1-t0))
 
 if __name__=="__main__":
-	for i in range(10):
-		bench_kv(100000,'write')
+	for i in range(10): bench_kv(100000,'keys')
 	#test_kv()
