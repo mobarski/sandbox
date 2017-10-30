a65bb984bf8dc676d48b05d55cc9ef30b4cb268e
Maciej Obarski <mobarski@gmail.com>
2017-10-30 22:46:45 +0100

    mp

--- mp/mp-usock-srv.py
+++ mp/mp-usock-srv.py
@@ -1,15 +1,21 @@
 import socket
+import marshal
+import os
 
 s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 s.bind('./unix-socket')
 s.listen(1)
 while True:
 	conn,addr = s.accept()
-	print(conn,addr)
+	print('incoming',addr)
+	fd = conn.fileno()
+	f = os.fdopen(fd)
 	while True:
-		data = conn.recv(16)
-		if data:
-			pass
-		else:
+		try:
+			data = marshal.load(f)
+		except EOFError:
 			break
+	f.close()
 	conn.close()
+	print(data)
+
