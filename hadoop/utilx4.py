095f252ab3ab7949c99f917d2b5950e50f7ac87b
Obarski Maciej <mobarski@pl.grupa.iti>
2017-12-11 09:48:37 +0100

    hadoop - fix utilx4

--- hadoop/utilx4.py
+++ hadoop/utilx4.py
@@ -2,7 +2,7 @@ import subprocess
 from textwrap import dedent
 from hashlib import sha1
 
-# version x4 - metadata support (column names)
+# version x4 - header, app_name, echo
 
 def pipe(*cmds, **kw):
 	doc = kw.get('doc',[])
@@ -16,8 +16,9 @@ def pipe(*cmds, **kw):
 
 
 class host:
-	def __init__(self, prefix='', run=False):
+	def __init__(self, prefix='', run=True, echo=True):
 		self._run = run
+		self._echo = echo
 		self.prefix = prefix.rstrip()+' ' if prefix else ''
 
 	def os(self, cmd, *a, **kw):
@@ -33,7 +34,8 @@ class host:
 		return self.run(full_cmd)
 
 	def run(self, cmd):
-		print(cmd)
+		if self._echo:
+			print(cmd)
 		if self._run:
 			return subprocess.check_call(cmd,shell=True)
 
@@ -65,40 +67,25 @@ class host:
 			self.os('rm -f '+script_path)
 	
 
-	def extract_csv(self, path, sql, output_dir='', script_path='', spark_args='', options='', remove='all'):
+	def extract_csv(self, path, sql, output_dir='', script_path='', app_name='utilx4', header=False, spark_args='', options='', remove='all'):
 		tmp_name = sha1(sql).hexdigest()[:16]
 		output_dir = output_dir or tmp_name
 		script_path = script_path or '/tmp/{0}.py'.format(tmp_name)
 		csv_options = '' if not options else ','+options
 		code = """
 			from pyspark.sql import SparkSession
-			spark = SparkSession.builder.appName('TODO').getOrCreate()
+			spark = SparkSession.builder.appName('{3}').getOrCreate()
 			df = spark.sql("{0}")
-			df.write.csv('{1}'{2})
-			# metadata
-			with open('{1}.meta','w') as f:
-				for col in df.columns:
-					f.write(col+'\n')
-			""".format(sql, output_dir, csv_options)
+			df.write.csv('{1}'{2},mode='overwrite',header={4})
+			""".format(sql, output_dir, csv_options, app_name, header)
 		
 		self.spark_run(code, script_path, spark_args=spark_args, remove=remove in ['all','script'])
 		
 		# TODO pipe into ???
 		self.dfs('text {0}/part* >{1} 2>{1}.log',output_dir,path) # getmerge?
-		self.dfs('text {0}.meta/part* >{1}.meta 2>>{1}.log',output_dir,path) # getmerge?
 		if remove.lower() in ['output','all']:
 			self.dfs('rm -r -f {0}',output_dir)
-			self.dfs('rm -r -f {0}.meta',output_dir)
 
 if __name__=="__main__":
 	h=host('ssh userxxx@aaa.bbb.ccc.ddd.pl',run=False)
-	h.dfs('put {0} {1}','xxx','yyy')
-	h.write('/tmp/xxx','to\njest\ntest')
-
-	#pipe(p.cat('../costam.txt'), h.put_pipe('/home/mobarki/xxx'))
-	#pipe(h.put('xxx','ttt'))
-	#pipe(h.spark2('xxx.py {0}','v13'))
-	#pipe(h.put('<<EOF','/tmp/costam.txt'),h.put('<<EOF','/tmp/xxx'),doc=['to\njest\ntest','EOF','zzz\nxxx','EOF'])
-
 	h.extract_csv('test.csv','select * from testdb.example_table limit 100',options="sep='|'")
-
