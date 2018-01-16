f28077ad0c87c6db49631efc0fb04ed71e440f60
mobarski <mobarski@gmail.com>
2018-01-17 00:17:27 +0100

    hadoop util x7x2 - new core class

--- hadoop/utilx7x2.py
+++ hadoop/utilx7x2.py
@@ -1,9 +1,22 @@
+import subprocess
+import tempfile
+
+# --- helpers
+def random_name(text='',text2='',length=6):
+	import random
+	import hashlib
+	out = hashlib.sha1(text).hexdigest()[:length]+'-'
+	out += hashlib.sha1(text2).hexdigest()[:length]+'-'
+	for i in range(length):
+		out += random.choice('qwertyuiopasdfghjklzxcvbnm1234567890')
+	return out
+
 class host:	
 	def __init__(self,ssh_cmd=''):
 		self.ssh_cmd = ssh_cmd
 		self.variable = {}
-		self.before = [] # TODO zawiera polecenia czy funkcje pythona?
-		self.script = []
+		self.before = []
+		self.main = []
 		self.after  = []
 	
 	def set(self,name,value):
@@ -11,44 +24,44 @@ class host:
 		self.variable[name] = value
 		return self
 	
-	def tmp(self,name,text):
+	def tmp(self,name,text='',eof='EOF',remove=True):
 		"create temporary file with given text"
-		path = '/tmp/'+self.random_name()
+		path = '/tmp/'+random_name(self.ssh_cmd,text)
 		self.variable[name] = path
-		self.before += ['cat >{0}'.format(path)] # TODO write(text.format(**self.variable))
-		self.after += ['rm {0}'.format(path)]
+		if text:
+			self.before += ['cat >{0} <<{2}\n{1}\n{2}'.format(path,text.format(**self.variable),eof)]
+		else:
+			self.before += ['touch {0}'.format(path)]
+		if remove:
+			self.after += ['rm {0}'.format(path)]
 		return self
 	
 	def cmd(self,text):
 		"execute command"
-		self.script += [text]
+		self.main += [text]
 		return self
 	
-	# --- helpers
-	def random_name(self,length=8):
-		import random
-		import hashlib
-		out = hashlib.sha1(self.ssh_cmd).hexdigest()[:length]+'-'
-		for i in range(length):
-			out += random.choice('qwertyuiopasdfghjklzxcvbnm1234567890')
-		return out
-
-	# ---
-	
-	def get(self):
+	def get_full_script(self):
 		before = '\n'.join(self.before).format(**self.variable)
-		script = '\n'.join(self.script).format(**self.variable)
+		main = '\n'.join(self.main).format(**self.variable)
 		after = '\n'.join(self.after).format(**self.variable)
-		return [before,script,after]
+		return '\n'.join([before,main,after]) + '\n'
 	
 	def run(self):
-		pass # TODO before
-		pass # TODO script
-		pass # TODO after
+		script = self.get_full_script()
+		with tempfile.TemporaryFile() as f:
+			f.write(script)
+			f.seek(0)
+			out = subprocess.check_output(self.ssh_cmd,stdin=f,shell=True) # TODO czy check_call stdout na stdout?
+		return out
 
-h=host()
-h.tmp('f1','xxx')
+h=host('cat >output.txt')
 h.set('hive','beeline -u "jdbc:hive2://xxx.aaa.bbb.ccc:10000/;principal=hive/xxx.aaa.bbb@zzz.vvv.bbb?mapreduce.job.queuename=abcd" --showHeader=false')
-h.cmd('{hive} -f {f1}')
-s = h.get()
+h.tmp('f1','xxx')
+h.tmp('f2','yyy')
+h.tmp('f3','zzz')
+h.tmp('f4',remove=False)
+h.cmd('{hive} -f {f1} >{f4}')
+s = h.get_full_script()
 print(s)
+h.run()
