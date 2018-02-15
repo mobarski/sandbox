ef12479e2d0260a1eb13d8ab60566534e75cb4f3
mobarski <mobarski@gmail.com>
2018-02-16 00:59:34 +0100

    ml

---
+++ ml/xgb/test1.py
@@ -0,0 +1,15 @@
+import xgboost as xgb
+import numpy as np
+
+dtrain = xgb.DMatrix('test1-train.txt')
+dtest  = xgb.DMatrix('test1-test.txt')
+
+options = {'silent':1}
+iters = 20
+bst = xgb.train(options, dtrain, iters, [(dtest,'test'),(dtrain,'train')])
+
+for x in ['weight','gain','cover']:
+	print(x,bst.get_score('',x))
+
+bst.save_model('test1.model')
+bst.dump_model('test1.model.txt')
