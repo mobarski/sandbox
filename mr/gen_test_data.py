8a08f400782054c075183db0b2d73952df218525
Maciej Obarski <mobarski@gmail.com>
2019-04-21 00:33:38 +0200

    mr - fixed mp

---
+++ mr/gen_test_data.py
@@ -0,0 +1,38 @@
+import sqlite3
+import re
+from random import randint
+
+# http://randomtextgenerator.com/
+text = """
+Perpetual sincerity out suspected necessary one but provision satisfied. Respect nothing use set waiting pursuit nay you looking. If on prevailed concluded ye abilities. Address say you new but minuter greater. Do denied agreed in innate. Can and middletons thoroughly themselves him. Tolerably sportsmen belonging in september no am immediate newspaper. Theirs expect dinner it pretty indeed having no of. Principle september she conveying did eat may extensive. 
+
+Entire any had depend and figure winter. Change stairs and men likely wisdom new happen piqued six. Now taken him timed sex world get. Enjoyed married an feeling delight pursuit as offered. As admire roused length likely played pretty to no. Means had joy miles her merry solid order. 
+
+Is at purse tried jokes china ready decay an. Small its shy way had woody downs power. To denoting admitted speaking learning my exercise so in. Procured shutters mr it feelings. To or three offer house begin taken am at. As dissuade cheerful overcame so of friendly he indulged unpacked. Alteration connection to so as collecting me. Difficult in delivered extensive at direction allowance. Alteration put use diminution can considered sentiments interested discretion. An seeing feebly stairs am branch income me unable. 
+
+Rooms oh fully taken by worse do. Points afraid but may end law lasted. Was out laughter raptures returned outweigh. Luckily cheered colonel me do we attacks on highest enabled. Tried law yet style child. Bore of true of no be deal. Frequently sufficient in be unaffected. The furnished she concluded depending procuring concealed. 
+
+Prepared is me marianne pleasure likewise debating. Wonder an unable except better stairs do ye admire. His and eat secure sex called esteem praise. So moreover as speedily differed branched ignorant. Tall are her knew poor now does then. Procured to contempt oh he raptures amounted occasion. One boy assure income spirit lovers set. 
+
+Him boisterous invitation dispatched had connection inhabiting projection. By mutual an mr danger garret edward an. Diverted as strictly exertion addition no disposal by stanhill. This call wife do so sigh no gate felt. You and abode spite order get. Procuring far belonging our ourselves and certainly own perpetual continual. It elsewhere of sometimes or my certainty. Lain no as five or at high. Everything travelling set how law literature. 
+
+Sudden she seeing garret far regard. By hardly it direct if pretty up regret. Ability thought enquire settled prudent you sir. Or easy knew sold on well come year. Something consulted age extremely end procuring. Collecting preference he inquietude projection me in by. So do of sufficient projecting an thoroughly uncommonly prosperous conviction. Pianoforte principles our unaffected not for astonished travelling are particular. 
+
+Considered an invitation do introduced sufficient understood instrument it. Of decisively friendship in as collecting at. No affixed be husband ye females brother garrets proceed. Least child who seven happy yet balls young. Discovery sweetness principle discourse shameless bed one excellent. Sentiments of surrounded friendship dispatched connection is he. Me or produce besides hastily up as pleased. Bore less when had and john shed hope. 
+
+Am terminated it excellence invitation projection as. She graceful shy believed distance use nay. Lively is people so basket ladies window expect. Supply as so period it enough income he genius. Themselves acceptance bed sympathize get dissimilar way admiration son. Design for are edward regret met lovers. This are calm case roof and. 
+
+Made last it seen went no just when of by. Occasional entreaties comparison me difficulty so themselves. At brother inquiry of offices without do my service. As particular to companions at sentiments. Weather however luckily enquire so certain do. Aware did stood was day under ask. Dearest affixed enquire on explain opinion he. Reached who the mrs joy offices pleased. Towards did colonel article any parties. 
+""" # 624 words
+
+sentences = re.split('[.]',text)
+sentences = [s.strip() for s in sentences]
+
+
+parts = [sentences[x::8] for x in range(8)]
+for i,part in enumerate(parts):
+	db = sqlite3.connect('data/{}'.format(i))
+	db.execute('drop table if exists test')
+	db.execute('create table if not exists test(x,text)')
+	db.executemany('insert into test values (?,?)',[(randint(1,4),p) for p in part])
+	db.commit()
---
+++ mr/mr1.py
@@ -0,0 +1,155 @@
+import sqlite3
+from time import time
+
+# TODO python combiner
+# TODO python combiner union
+# TODO grouping
+# TODO parameter placeholders
+
+def _map_fun(sql, part, path, combiner, functions, aggregates):
+	t0=time()
+	db = sqlite3.connect(path)
+	
+	for x in functions:
+		db.create_function(*x)
+	for x in aggregates:
+		db.create_aggregate(*x)
+	
+	result = db.execute(sql)
+	out = {}
+	out['rows'] = list(result)
+	out['cols'] = [x[0] for x in result.description]
+	out['part'] = part
+	out['map_time'] = time()-t0
+	if combiner:
+		t0=time()
+		out = combiner(out)
+		out['combine_time'] = time()-t0
+	return out
+
+
+def _map_fun1(args):
+	return _map_fun(*args)
+
+
+def map_sql(sql, parts, path_fun, pool=None, combiner=None, functions=[], aggregates=[]):
+	args = [(sql,part,path_fun(part),combiner,functions,aggregates) for part in parts]
+	if pool:
+		out = pool.map(_map_fun1, args)
+	else:
+		out = map(_map_fun1, args)
+	return out
+
+
+def union(map_output):
+	tab = 'part' # TODO rename ???
+	cols = map_output[0]['cols']
+	db=sqlite3.connect(':memory:')
+	db.execute('create table {}({})'.format(tab, ','.join(cols)))
+	for p in map_output:
+		sql = 'insert into {} values({})'.format(tab, ','.join(['?']*len(cols)))
+		db.executemany(sql,p['rows'])
+	return db
+
+def reduce_sql(sql, map_output):
+	db = union(map_output)
+	return db.execute(sql)
+
+# ------------------------------------------------------------------------------
+
+if __name__=="xxx__main__":
+	import multiprocessing as mp
+	pool = mp.Pool(2)
+
+	def get_part_path(part):
+		return 'data/{}'.format(part)
+
+	out = map_sql("""
+			select x, count(1) as count
+			from test
+			group by x
+		"""
+		,[0,1,2,3,4,5,6,7]
+		,get_part_path
+		#,pool=pool
+		)
+
+	for x in reduce_sql("""
+				select x, sum(count) as count
+				from part group by x
+				order by count desc
+			""", out
+			):
+		print(x)
+
+
+# ------------------------------------------------------------------------------
+
+#from marshal import dumps,loads
+from pickle import dumps,loads
+from collections import Counter
+import re
+
+
+def tf(map_fun_output):
+	out = map_fun_output
+	
+	tf = Counter()
+	for row in out['rows']:
+		tokens = re.findall('(?u)\w+',row[0])
+		tf.update(tokens)
+	if 1:
+		out['cols'] = ['group','tf']
+		out['rows'] = [('all',dict(tf))]
+	else:
+		out['cols'] = ['term','freq']
+		out['rows'] = tf.items()
+	return out
+
+class TF:
+	def __init__(self):
+		self.tf = Counter()
+	def step(self, value):
+		tokens = re.findall('(?u)\w+',value)
+		self.tf.update(tokens)
+	def finalize(self):
+		#return buffer(dumps(dict(self.tf))) # marshal -> pool.map hangs
+		return dumps(dict(self.tf)) # pickle -> ok
+
+if __name__=="__main__":
+	from pprint import pprint
+	import multiprocessing as mp
+	pool = mp.Pool(2)
+	
+	def get_part_path(part):
+		return 'data/{}'.format(part)
+
+	for i in range(2):
+		if 1:
+			t0=time()
+			out = map_sql("""
+					select lower(text)
+					from test
+				"""
+				,[0,1,2,3,4,5,6,7]
+				,get_part_path
+				,combiner=tf
+				,pool=pool
+				)
+			print(time()-t0, sum([sum(p['rows'][0][1].values()) for p in out]))
+
+		if 1:
+			t0=time()
+			out = map_sql("""
+					select tf(lower(text)) as tf
+					from test
+				"""
+				,[0,1,2,3,4,5,6,7]
+				,get_part_path
+				,aggregates=[('tf',1,TF)]
+				,pool=pool
+				)
+			print(time()-t0, sum([sum(loads(p['rows'][0][0]).values()) for p in out]))
+	
+	pool.close()
+	pool.join()
