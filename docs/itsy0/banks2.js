3fcb0c1e4f10ec450e1e768976ccaea1e719cefe
Maciej Obarski <mobarski@gmail.com>
2019-10-23 15:53:14 +0200

    itsy - packed_array

--- docs/itsy0/banks2.js
+++ docs/itsy0/banks2.js
@@ -20,3 +20,53 @@ function _compact_imagedate_to_array(img,n) {
 	}
 	return ar
 }
+
+function _array_to_packed_array(ar,max_v) {
+	var out = []
+	var n = 1
+	var k = 0
+	if (max_v==1) 				n=8
+	if (max_v>1 && max_v<4) 	n=4
+	if (max_v>3 && max_v<16) 	n=2
+	
+	var val = 0
+	var j
+	for (var i in ar) {
+		j = i%n
+		var v = max(0,min(ar[i],max_v))
+		val |= v
+		if (j==n-1) {
+			out.push(val)
+			val = 0
+		} else {
+			val <<= 8/n
+		}
+	}
+	if (j != n-1) {
+		out.push(val)
+	}
+	return out
+}
+
+function _packed_array_to_array(ar,max_v) {
+	var out = []
+	var m = 255
+	var n = 1
+	if (max_v==1) 				{n=8;m=1}
+	if (max_v>1 && max_v<4) 	{n=4;m=3}
+	if (max_v>3 && max_v<16) 	{n=2;m=15}
+
+	for (var i in ar) {
+		var batch = []
+		var v = ar[i]
+		for (var j=0; j<n; j++) {
+			var val = v&m
+			batch.unshift(val)
+			v >>= 8/n
+		}
+		for (var j in batch) {
+			out.push(batch[j])
+		}
+	}
+	return out
+}
